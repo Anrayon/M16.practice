916e57530da308e934336efdd5bb0a3590060019
Andrey Slyusarchuk <anray.nr@gmail.com>
2022-10-12 19:45:28 +0300

    Initial Commit

---
+++ M16.practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M16.practice
+//
+//  Created by Andrey on 12.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
